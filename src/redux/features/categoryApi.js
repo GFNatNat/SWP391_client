97e9cb06d839c056df1a7046924ef25ddeecf21c
GFNatNat <127825172+GFNatNat@users.noreply.github.com>
2024-06-19 13:17:03 +0700

    update backend uri

--- src/redux/features/categoryApi.js
+++ src/redux/features/categoryApi.js
@@ -5,16 +5,16 @@ export const categoryApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     addCategory: builder.mutation({
       query: (data) => ({
-        url: "https://shofy-backend.vercel.app/api/category/add",
+        url: "https://swp391serversided-git-main-gfnatnats-projects.vercel.app/api/category/add",
         method: "POST",
         body: data,
       }),
     }),
     getShowCategory: builder.query({
-      query: () => `https://shofy-backend.vercel.app/api/category/show`
+      query: () => `https://swp391serversided-git-main-gfnatnats-projects.vercel.app/api/category/show`
     }),
     getProductTypeCategory: builder.query({
-      query: (type) => `https://shofy-backend.vercel.app/api/category/show/${type}`
+      query: (type) => `https://swp391serversided-git-main-gfnatnats-projects.vercel.app/api/category/show/${type}`
     }),
   }),
 });
